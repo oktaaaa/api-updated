b22a283101c61bca6eba4924e637bc96846ae91a
oktaaaa <oktavianieps@gmail.com>
2023-10-28 12:53:00 +0700

    gitignore vercel

--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -52,4 +52,24 @@ router.delete('/:ID', async(req, res)=>{
     }
 })
 
+// edit
+router.put('/:ID', async(req, res) => {
+    const dataMahasiswa = {
+        npm: req.body.npm,
+        nama: req.body.nama,
+        jk: req.body.jk,
+        tanggal: req.body.tanggal,
+        prodi: req.body.prodi,
+        dosenpa: req.body.dosenpa
+    }
+    try {
+        const mahasiswa = await Mahasiswa.updateOne({
+            _id: req.params.ID
+        }, dataMahasiswa)
+        res.json(mahasiswa)
+    } catch (error) {
+        res.json({message: error})
+    }
+})
+
 module.exports = router
